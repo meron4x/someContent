891180d1269e3010ec7e1d28278e8e3ad20fe506
meronmikel <meronmkl@gmail.com>
2021-08-16 18:46:28 +0200

    major changes with view buttons

--- js.js
+++ js.js
@@ -1,2 +1,56 @@
-var today = new Date();
-var date = today. getFullYear()+'-'+(today. getMonth()+1)+'-'+today. getDate();
+
+var gridCss = '<link rel="stylesheet" href="gridview.css" type="text/css">';
+var headinnerHTML = document.querySelector('link').innerHTML;
+var flexCss = '<link rel="stylesheet" href="gridview.css" type="text/css">'
+
+var gridview = false;
+
+document.getElementById("mygridbtn").addEventListener("click", function(){ 
+  if (gridview == false) {
+    document.querySelector("link[href='styleintro.css']").href = "gridview.css";
+    gridview = true;
+  }
+});
+document.getElementById("myflexbtn").addEventListener("click", function(){ 
+  if (gridview == true) {
+    document.querySelector("link[href='gridview.css']").href = "flexview.css";
+    gridview = false;
+  }
+});
+
+ /*
+  document.querySelector('head').innerHTML += '<link rel="stylesheet" href="styles.css" type="text/css"/>';
+
+
+  if (document.styleSheets[0].href = "style.css") {
+    x = 1+1;
+    alert("terekiba")
+  }
+  else {
+    alert('ayterekbern')
+  }
+
+
+  **********************************
+
+var headinnerHTML = document.querySelector('link').innerHTML;
+var gridCss = '<link rel="stylesheet" href="gridview.css" type="text/css">';
+var flexCss = '<link rel="stylesheet" href="gridview.css" type="text/css">'
+var gridview = false;
+
+document.getElementById("mygridbtn").addEventListener("click", function(){ 
+  if (gridview == false) {
+   headinnerHTML -= flexCss;
+   headinnerHTML += gridCss;
+    gridview = true;
+  }
+});
+document.getElementById("myflexbtn").addEventListener("click", function(){ 
+  if (gridview == true) {
+    headinnerHTML -= gridCss;
+    headinnerHTML += flexCss;
+    gridview = false;
+  }
+});
+
+  */
