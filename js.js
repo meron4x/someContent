182830e432d608e0f3b9946d6aa39f4c90ac5c38
Meron Habtemichael <meronmkl@gmail.com>
2021-11-20 03:54:37 +0100

    x

--- js.js
+++ js.js
@@ -1,10 +1,4 @@
-
-var gridCss = '<link rel="stylesheet" href="gridview.css" type="text/css">';
-var headinnerHTML = document.querySelector('link').innerHTML;
-var flexCss = '<link rel="stylesheet" href="gridview.css" type="text/css">'
-
 var gridview = false;
-
 document.getElementById("mygridbtn").addEventListener("click", function(){ 
   if (gridview == false) {
     document.querySelector("link[href='flexview.css']").href = "gridview.css";
@@ -17,40 +11,3 @@ document.getElementById("myflexbtn").addEventListener("click", function(){
     gridview = false;
   }
 });
-
- /*
-  document.querySelector('head').innerHTML += '<link rel="stylesheet" href="styles.css" type="text/css"/>';
-
-
-  if (document.styleSheets[0].href = "style.css") {
-    x = 1+1;
-    alert("terekiba")
-  }
-  else {
-    alert('ayterekbern')
-  }
-
-
-  **********************************
-
-var headinnerHTML = document.querySelector('link').innerHTML;
-var gridCss = '<link rel="stylesheet" href="gridview.css" type="text/css">';
-var flexCss = '<link rel="stylesheet" href="gridview.css" type="text/css">'
-var gridview = false;
-
-document.getElementById("mygridbtn").addEventListener("click", function(){ 
-  if (gridview == false) {
-   headinnerHTML -= flexCss;
-   headinnerHTML += gridCss;
-    gridview = true;
-  }
-});
-document.getElementById("myflexbtn").addEventListener("click", function(){ 
-  if (gridview == true) {
-    headinnerHTML -= gridCss;
-    headinnerHTML += flexCss;
-    gridview = false;
-  }
-});
-
-  */
